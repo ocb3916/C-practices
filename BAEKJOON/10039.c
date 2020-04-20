7664e1760f54b1a2e3bd51fb90dac23251c70b0b
ocb3916 <dhchanbin1004@gmail.com>
2020-04-20 23:56:08 +0900

    C practice

---
+++ BAEKJOON/10039.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, d, e;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &d);
+	scanf("%d", &e);
+
+	if (a < 40)
+		a = 40;
+	if (b < 40)
+		b = 40;
+	if (c < 40)
+		c = 40;
+	if (d < 40)
+		d = 40;
+	if (e < 40)
+		e = 40;
+
+	printf("%d", (a + b + c + d + e) / 5);
+
+	return 0;
+}
